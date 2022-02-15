633f7ae66aaf654226ce9b24d7aa4c1d5c1316e5
Totobird <>
2022-02-15 07:43:59 -0500

    Updated: Flat generator;

--- src/game/world/generator/flat.rs
+++ src/game/world/generator/flat.rs
@@ -13,8 +13,8 @@ impl Generator for FlatGenerator {
     fn generate_chunk(&self, _chunk : [i32; 3], _origin : [i32; 3]) -> ChunkArray {
         let mut array = ChunkArray::new();
         let     size  = array.get_size().clone();
-        for ix in 1..size[0] {
-            for iz in 1..size[2] {
+        for ix in 0..size[0] {
+            for iz in 0..size[2] {
                 array.push([ix, 0, iz], &voxels::SOLID);
                 if (ix == iz) {
                     array.push([ix, 1, iz], &voxels::SOLID);
